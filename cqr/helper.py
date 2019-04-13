0ca108a7c48f369925c8fdb80e4768c78a08d4fc
Yaniv Romano <romano@DN0a22c9e3.SUNet>
2019-04-12 18:20:02 -0700

    small changes

--- cqr/helper.py
+++ cqr/helper.py
@@ -17,20 +17,20 @@ else:
 
 def compute_coverage_len(y_test, y_lower, y_upper):
     """ Compute average coverage and length of prediction intervals
-    
+
     Parameters
     ----------
-    
+
     y_test : numpy array, true labels (n)
     y_lower : numpy array, estimated lower bound for the labels (n)
     y_upper : numpy array, estimated upper bound for the labels (n)
-    
+
     Returns
     -------
-    
+
     coverage : float, average coverage
     avg_length : float, average length
-    
+
     """
     in_the_range = np.sum((y_test >= y_lower) & (y_test <= y_upper))
     coverage = in_the_range / len(y_test) * 100
@@ -39,10 +39,10 @@ def compute_coverage_len(y_test, y_lower, y_upper):
 
 def run_icp(nc, X_train, y_train, X_test, idx_train, idx_cal, significance):
     """ Run split conformal method
-    
+
     Parameters
     ----------
-    
+
     nc : class of nonconformist object
     X_train : numpy array, training features (n1Xp)
     y_train : numpy array, training labels (n1)
@@ -50,13 +50,13 @@ def run_icp(nc, X_train, y_train, X_test, idx_train, idx_cal, significance):
     idx_train : numpy array, indices of proper training set examples
     idx_cal : numpy array, indices of calibration set examples
     significance : float, significance level (e.g. 0.1)
-    
+
     Returns
     -------
-    
+
     y_lower : numpy array, estimated lower bound for the labels (n2)
     y_upper : numpy array, estimated upper bound for the labels (n2)
-    
+
     """
     icp = IcpRegressor(nc)
 
@@ -76,22 +76,22 @@ def run_icp(nc, X_train, y_train, X_test, idx_train, idx_cal, significance):
 
 def compute_coverage(y_test,y_lower,y_upper,significance,name=""):
     """ Compute average coverage and length, and print results
-    
+
     Parameters
     ----------
-    
+
     y_test : numpy array, true labels (n)
     y_lower : numpy array, estimated lower bound for the labels (n)
     y_upper : numpy array, estimated upper bound for the labels (n)
     significance : float, desired significance level
     name : string, optional output string (e.g. the method name)
-    
+
     Returns
     -------
-    
+
     coverage : float, average coverage
     avg_length : float, average length
-    
+
     """
     in_the_range = np.sum((y_test >= y_lower) & (y_test <= y_upper))
     coverage = in_the_range / len(y_test) * 100
@@ -105,17 +105,17 @@ def compute_coverage(y_test,y_lower,y_upper,significance,name=""):
 
 def plot_func_data(y_test,y_lower,y_upper,name=""):
     """ Plot the test labels along with the constructed prediction band
-    
+
     Parameters
     ----------
-    
+
     y_test : numpy array, true labels (n)
     y_lower : numpy array, estimated lower bound for the labels (n)
     y_upper : numpy array, estimated upper bound for the labels (n)
     name : string, optional output string (e.g. the method name)
-    
+
     """
-    
+
     # allowed to import graphics
     import matplotlib.pyplot as plt
 
@@ -178,9 +178,9 @@ class MSENet_RegressorAdapter(RegressorAdapter):
                  wd=1e-6,
                  test_ratio=0.2,
                  random_state=0):
-        
+
         """ Initialization
-    
+
         Parameters
         ----------
         model : unused parameter (for compatibility with nc class)
@@ -195,7 +195,7 @@ class MSENet_RegressorAdapter(RegressorAdapter):
         wd : float, weight decay
         test_ratio : float, ratio of held-out data, used in cross-validation
         random_state : integer, seed for splitting the data in cross-validation
-        
+
         """
         super(MSENet_RegressorAdapter, self).__init__(model, fit_params)
         # Instantiate model
@@ -217,19 +217,19 @@ class MSENet_RegressorAdapter(RegressorAdapter):
 
     def fit(self, x, y):
         """ Fit the model to data
-    
+
         Parameters
         ----------
-        
+
         x : numpy array of training features (nXp)
         y : numpy array of training labels (n)
-        
+
         """
         self.learner.fit(x, y, self.epochs, batch_size=self.batch_size)
 
     def predict(self, x):
         """ Estimate the label given the features
-    
+
         Parameters
         ----------
         x : numpy array of training features (nXp)
@@ -237,7 +237,7 @@ class MSENet_RegressorAdapter(RegressorAdapter):
         Returns
         -------
         ret_val : numpy array of predicted labels (n)
-        
+
         """
         return self.learner.predict(x)
 
@@ -245,7 +245,7 @@ class MSENet_RegressorAdapter(RegressorAdapter):
 # Deep neural network for conditional quantile regression
 # Minimizing pinball loss
 ###############################################################################
-        
+
 class AllQNet_RegressorAdapter(RegressorAdapter):
     """ Conditional quantile estimator, formulated as neural net
     """
@@ -265,7 +265,7 @@ class AllQNet_RegressorAdapter(RegressorAdapter):
                  random_state=0,
                  use_rearrangement=False):
         """ Initialization
-    
+
         Parameters
         ----------
         model : None, unused parameter (for compatibility with nc class)
@@ -281,15 +281,15 @@ class AllQNet_RegressorAdapter(RegressorAdapter):
         wd : float, weight decay
         test_ratio : float, ratio of held-out data, used in cross-validation
         random_state : integer, seed for splitting the data in cross-validation
-        use_rearrangement : boolean, use the rearangement algorithm (True)
+        use_rearrangement : boolean, use the rearrangement algorithm (True)
                             of not (False). See reference [1].
-                            
+
         References
         ----------
-        .. [1]  Chernozhukov, Victor, Iván Fernández‐Val, and Alfred Galichon. 
-                "Quantile and probability curves without crossing." 
+        .. [1]  Chernozhukov, Victor, Iván Fernández‐Val, and Alfred Galichon.
+                "Quantile and probability curves without crossing."
                 Econometrica 78.3 (2010): 1093-1125.
-        
+
         """
         super(AllQNet_RegressorAdapter, self).__init__(model, fit_params)
         # Instantiate model
@@ -322,19 +322,19 @@ class AllQNet_RegressorAdapter(RegressorAdapter):
 
     def fit(self, x, y):
         """ Fit the model to data
-    
+
         Parameters
         ----------
-        
+
         x : numpy array of training features (nXp)
         y : numpy array of training labels (n)
-        
+
         """
         self.learner.fit(x, y, self.epochs, self.batch_size)
 
     def predict(self, x):
         """ Estimate the conditional low and high quantiles given the features
-    
+
         Parameters
         ----------
         x : numpy array of training features (nXp)
@@ -342,7 +342,7 @@ class AllQNet_RegressorAdapter(RegressorAdapter):
         Returns
         -------
         ret_val : numpy array of estimated conditional quantiles (nX2)
-        
+
         """
         return self.learner.predict(x)
 
@@ -350,42 +350,42 @@ class AllQNet_RegressorAdapter(RegressorAdapter):
 ###############################################################################
 # Quantile random forests model
 ###############################################################################
-        
+
 class QuantileForestRegressorAdapter(RegressorAdapter):
     """ Conditional quantile estimator, defined as quantile random forests (QRF)
-    
+
     References
     ----------
     .. [1]  Meinshausen, Nicolai. "Quantile regression forests."
             Journal of Machine Learning Research 7.Jun (2006): 983-999.
-                
+
     """
-    
+
     def __init__(self,
                  model,
                  fit_params=None,
                  quantiles=[5, 95],
                  params=None):
         """ Initialization
-    
+
         Parameters
         ----------
         model : None, unused parameter (for compatibility with nc class)
         fit_params : None, unused parameter (for compatibility with nc class)
         quantiles : numpy array, low and high quantile levels in range (0,100)
         params : dictionary of parameters
-                params["random_state"] : integer, seed for splitting the data 
+                params["random_state"] : integer, seed for splitting the data
                                          in cross-validation. Also used as the
                                          seed in quantile random forests (QRF)
                 params["min_samples_leaf"] : integer, parameter of QRF
                 params["n_estimators"] : integer, parameter of QRF
                 params["max_features"] : integer, parameter of QRF
-                params["CV"] : boolean, use cross-validation (True) or 
+                params["CV"] : boolean, use cross-validation (True) or
                                not (False) to tune the two QRF quantile levels
                                to obtain the desired coverage
                 params["test_ratio"] : float, ratio of held-out data, used
                                        in cross-validation
-                params["coverage_factor"] : float, to avoid too consevative
+                params["coverage_factor"] : float, to avoid too conservative
                                             estimation of the prediction band,
                                             when tuning the two QRF quantile
                                             levels in cross-validation one may
@@ -393,16 +393,16 @@ class QuantileForestRegressorAdapter(RegressorAdapter):
                                             reduced average coverage, equal to
                                             coverage_factor*(q_high - q_low).
                 params["range_vals"] : float, determines the lowest and highest
-                                       quantile level parameters when tuning 
+                                       quantile level parameters when tuning
                                        the quanitle levels bt cross-validation.
                                        The smallest value is equal to
                                        quantiles[0] - range_vals.
-                                       Similarly, the largest is equal to 
+                                       Similarly, the largest is equal to
                                        quantiles[1] + range_vals.
                 params["num_vals"] : integer, when tuning QRF's quantile
                                      parameters, sweep over a grid of length
                                      num_vals.
-        
+
         """
         super(QuantileForestRegressorAdapter, self).__init__(model, fit_params)
         # Instantiate model
@@ -416,13 +416,13 @@ class QuantileForestRegressorAdapter(RegressorAdapter):
 
     def fit(self, x, y):
         """ Fit the model to data
-    
+
         Parameters
         ----------
-        
+
         x : numpy array of training features (nXp)
         y : numpy array of training labels (n)
-        
+
         """
         if self.params["CV"]:
             target_coverage = self.quantiles[1] - self.quantiles[0]
@@ -446,7 +446,7 @@ class QuantileForestRegressorAdapter(RegressorAdapter):
 
     def predict(self, x):
         """ Estimate the conditional low and high quantiles given the features
-    
+
         Parameters
         ----------
         x : numpy array of training features (nXp)
@@ -454,7 +454,7 @@ class QuantileForestRegressorAdapter(RegressorAdapter):
         Returns
         -------
         ret_val : numpy array of estimated conditional quantiles (nX2)
-        
+
         """
         lower = self.rfqr.predict(x, quantile=self.cv_quantiles[0])
         upper = self.rfqr.predict(x, quantile=self.cv_quantiles[1])
@@ -463,5 +463,3 @@ class QuantileForestRegressorAdapter(RegressorAdapter):
         ret_val[:,0] = lower
         ret_val[:,1] = upper
         return ret_val
-    
-
